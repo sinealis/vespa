2249dbf7a05c575650f339784aabb1fe001907f1
sinealis <judita.csibova@gmail.com>
2021-07-01 19:26:52 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+new fullpage("#fullpage", {
+	autoScrolling: true,
+	navigation: true,
+	onLeave: (origin, destination, direction) => {
+		const section = destination.item;
+		const title = section.querySelectorAll("h1, h2");
+		const tl = new TimelineMax({ delay: 0.2 });
+		const vespa = document.querySelectorAll(".vespa");
+		const description = document.querySelector(".description");
+
+		tl.fromTo(title, 0.5, { y: 50, opacity: 0 }, { y: 0, opacity: 1 });
+
+		// s2 section
+		if (destination.index === 1) {
+			tl.fromTo(vespa, 0.4, { x: "500%", opacity: 0 }, { x: "-45%", opacity: 1 })
+				.fromTo(description, 0.3, { y: 50, opacity: 0 }, { y: 0, opacity: 1 });
+		}
+		// s3 section
+		if (destination.index === 2) {
+			tl.fromTo(vespa, 0.4, { x: "-500%", opacity: 0 }, { x: "-45%", opacity: 1 })
+				.fromTo(description, 0.3, { y: -50, opacity: 0 }, { y: 0, opacity: 1 });
+		}
+		// s4 section
+		if (destination.index === 3) {
+			tl.fromTo(vespa, 0.4, { x: "500%", opacity: 0 }, { x: "-45%", opacity: 1 })
+				.fromTo(description, 0.3, { y: 50, opacity: 0 }, { y: 0, opacity: 1 });
+		}
+	}
+});
