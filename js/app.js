fd635e99fdfc2c6bf514dd83324fb790284ec3e3
Judit Csiba <71337087+sinealis@users.noreply.github.com>
2021-08-12 15:42:18 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ new fullpage("#fullpage", {
 	onLeave: (origin, destination, direction) => {
 		const section = destination.item;
 		const title = section.querySelectorAll("h1, h2");
-		const tl = new TimelineMax({ delay: 0.2 });
+		const tl = new TimelineMax({ delay: 0.3 });
 		const vespa = document.querySelectorAll(".vespa");
 		const description = document.querySelector(".description");
 
@@ -12,18 +12,18 @@ new fullpage("#fullpage", {
 
 		// s2 section
 		if (destination.index === 1) {
-			tl.fromTo(vespa, 0.4, { x: "500%", opacity: 0 }, { x: "-45%", opacity: 1 })
-				.fromTo(description, 0.3, { y: 50, opacity: 0 }, { y: 0, opacity: 1 });
+			tl.fromTo(vespa, 0.5, { x: "500%", opacity: 0 }, { x: "-45%", opacity: 1 })
+				.fromTo(description, 0.4, { y: 50, opacity: 0 }, { y: 0, opacity: 1 });
 		}
 		// s3 section
 		if (destination.index === 2) {
-			tl.fromTo(vespa, 0.4, { x: "-500%", opacity: 0 }, { x: "-45%", opacity: 1 })
-				.fromTo(description, 0.3, { y: -50, opacity: 0 }, { y: 0, opacity: 1 });
+			tl.fromTo(vespa, 0.5, { x: "-500%", opacity: 0 }, { x: "-45%", opacity: 1 })
+				.fromTo(description, 0.4, { y: -50, opacity: 0 }, { y: 0, opacity: 1 });
 		}
 		// s4 section
 		if (destination.index === 3) {
-			tl.fromTo(vespa, 0.4, { x: "500%", opacity: 0 }, { x: "-45%", opacity: 1 })
-				.fromTo(description, 0.3, { y: 50, opacity: 0 }, { y: 0, opacity: 1 });
+			tl.fromTo(vespa, 0.5, { x: "500%", opacity: 0 }, { x: "-45%", opacity: 1 })
+				.fromTo(description, 0.4, { y: 50, opacity: 0 }, { y: 0, opacity: 1 });
 		}
 	}
 });
